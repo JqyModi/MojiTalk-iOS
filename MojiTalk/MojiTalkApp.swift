93454d2e0db8b97d4e5b48b066803b05131402ae
jqy <jiangqiyu@shareintelli.com>
2026-02-04 00:17:31 +0800

    Initial Commit

---
+++ MojiTalk/MojiTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MojiTalkApp.swift
+//  MojiTalk
+//
+//  Created by J.qy on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct MojiTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
